2abe1f23453823ec826e0648d76324f8dc65ceb2
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-04 03:16:57 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,16 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  pp nds
 end
 
 def print_first_directors_movie_titles
+  nds = directors_database
+  titles = ""
+  for each in nds[0][:movies] do
+    titles = titles + (each[:title] + "\n")  
+  end
+  print titles
 end
+
+p print_first_directors_movie_titles
