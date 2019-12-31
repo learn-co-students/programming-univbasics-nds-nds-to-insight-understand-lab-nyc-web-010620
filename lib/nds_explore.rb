4a4bfe2de7daac1d03dc24bbe5fb2f4638937259
Adam Xiao <adam.x.xiao@gmail.com>
2019-12-31 02:07:12 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,16 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+    require 'pp'
+    pp nds
 end
 
 def print_first_directors_movie_titles
+  #puts directors_database[0][:movies].length
+
+  movie_name = 0
+  while movie_name < directors_database[0][:movies].length
+    puts "#{directors_database[0][:movies][movie_name][:title]}"
+    movie_name += 1
+  end
 end
